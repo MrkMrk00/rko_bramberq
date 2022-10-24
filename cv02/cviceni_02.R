342173e9e65c0b8b3aa678ea7d2e1720b79e02b9
MrkMrk00 <mhau@outlook.cz>
2022-10-24 13:23:53 +0200

    02: 1-6

--- cv02/cviceni_02.R
+++ cv02/cviceni_02.R
@@ -25,3 +25,30 @@ sd(data$Showertime)
 #    0%  25%  50%  75% 100%
 #    16  100  175  294 2047
 quantile(data$Showertime)
+
+require('dplyr')
+shower_data <- data.frame(Hh_ID = c(1,1,1,2,2), Volume = c(30, 33, 23, 40, 35))
+shower_data[shower_data$Volume %in% c(30, 35),]
+
+# Excercise I
+# 1)
+# a <- data[data$Hh_ID == 6395,]
+a <- filter(data, Hh_ID == 6395)
+
+# 2)
+b <- arrange(data[data$Hh_ID == 6395,], Volume)
+
+# 3)
+c <- filter(data, !(Hh_ID %in% c(6395, 5307)))
+
+# Excercise II
+# 1)
+# min(data$Showertime)
+# max(data$Showertime)
+summarise(data, maxVol = max(Showertime), minVol = min(Showertime))
+
+# 2)
+require('weathermetrics')
+mutate(data, Avgtemperaturefahrenheit = celsius.to.fahrenheit(Avgtemperature))
+
+# 3)
