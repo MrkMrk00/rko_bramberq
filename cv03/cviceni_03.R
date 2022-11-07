545a523d70242def637b061689e2af4e3dc45229
MrkMrk00 <mhau@outlook.cz>
2022-11-07 13:11:55 +0100

    saegth

--- cv03/cviceni_03.R
+++ cv03/cviceni_03.R
@@ -1,5 +1,3 @@
 data <- read.csv2('./cv02/01_dataset_Shower_data.csv')
-require("dplyr")
+data$group <- as.factor(data$group)
 
-head(data)
-summarise(data, minDur = min(Showertime), maxDur = max(Showertime))
